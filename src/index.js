8bef705a118732045f42a38a45ee43c7073946bf
dangreen <danon0404@gmail.com>
2026-07-06 20:30:38 +0400

    feat!: require name argument in greetings
    BREAKING CHANGE: `greet` and `farewell` no longer default to `world` and throw when the name is not provided.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
-export function greet(name = 'world') {
+export function greet(name) {
+  if (!name) {
+    throw new Error('Name is required')
+  }
+
   return `Hello, ${name}!`
 }
 
-export function farewell(name = 'world') {
+export function farewell(name) {
+  if (!name) {
+    throw new Error('Name is required')
+  }
+
   return `Goodbye, ${name}!`
 }
